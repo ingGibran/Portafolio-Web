3c5a9fa1ad456491fe01752d9285a5430d0ff3f8
ingGibran <zaga1709@hotmail.com>
2025-12-01 10:45:21 -0600

    no migraciones

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "Instalando dependencias..."
 pip install -r requirements.txt
 
 echo "Aplicando migraciones..."
-python manage.py migrate --noinput
+# python manage.py migrate --noinput   <-- comentar esta línea
 
 echo "Recolectando estáticos..."
 python manage.py collectstatic --noinput --clear
